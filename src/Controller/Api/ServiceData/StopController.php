4c8f021533cdad09ffdf7eca38993970638eaf81
Berto Ferrero <berto@prestaquality.com>
2022-03-09 22:09:35 +0100

    Consulta de paradas por stopcode

--- src/Controller/Api/ServiceData/StopController.php
+++ src/Controller/Api/ServiceData/StopController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\ServiceData\Route as ServiceDataRoute;
 use App\Lib\Helpers\DateTimeHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/stop')]
@@ -43,40 +44,68 @@ class StopController extends AbstractController
      * Gets one stop
      */
     #[Route('/{schema_id}', name: 'api_stop_get_one', methods: ['GET'])]
-    public function getOneAction(string $schema_id, EntityManagerInterface $em): Response
+    public function getOneAction(string $schema_id, EntityManagerInterface $em, Request $request, DateTimeHelper $dateTimeHelper): Response
     {
         $stop = $em->getRepository(Stop::class)->findBySchemaId($schema_id);
         if (empty($stop)) {
             return $this->json(null, Response::HTTP_NOT_FOUND);
         }
-        return $this->json([
+        $data = [
             'id' => $stop->getSchemaId(),
             'lat' => $stop->getLatitude(),
             'lng' => $stop->getLongitude(),
             'name' => $stop->getName(),
             'code' => $stop->getCode()
-        ]);
+        ];
+        if ($request->query->get('withRoutes', 0) == 1) {
+            $data['routes'] = $this->getRoutesForStop($stop, $em, $dateTimeHelper);
+        }
+        return $this->json($data);
+    }
+    
+    #[Route('/stopcode/{stop_code}', name: 'api_stop_get_one_stop_code', methods: ['GET'])]
+    public function getOneStopCodeAction(string $stop_code, EntityManagerInterface $em, Request $request, DateTimeHelper $dateTimeHelper): Response
+    {
+        $stop = $em->getRepository(Stop::class)->findOneBy(['code' => $stop_code]);
+        if (empty($stop)) {
+            return $this->json(null, Response::HTTP_NOT_FOUND);
+        }
+        $data = [
+            'id' => $stop->getSchemaId(),
+            'lat' => $stop->getLatitude(),
+            'lng' => $stop->getLongitude(),
+            'name' => $stop->getName(),
+            'code' => $stop->getCode()
+        ];
+        if ($request->query->get('withRoutes', 0) == 1) {
+            $data['routes'] = $this->getRoutesForStop($stop, $em, $dateTimeHelper);
+        }
+        return $this->json($data);
     }
 
     /**
      * Gets one stop by latitude and longitude
      */
     #[Route('/nearest/{latitude}/{longitude}', name: 'api_stop_get_one_gps', methods: ['GET'])]
-    public function getOneGpsAction(float $latitude, float $longitude, EntityManagerInterface $em): Response
+    public function getOneGpsAction(float $latitude, float $longitude, EntityManagerInterface $em, Request $request, DateTimeHelper $dateTimeHelper): Response
     {
         $distance = 0;
         $stop = $em->getRepository(Stop::class)->findByLatitudeLongitude($latitude, $longitude, 0, $distance);
         if (empty($stop)) {
             return $this->json(null, Response::HTTP_NOT_FOUND);
         }
-        return $this->json([
+        $data = [
             'id' => $stop->getSchemaId(),
             'lat' => $stop->getLatitude(),
             'lng' => $stop->getLongitude(),
             'name' => $stop->getName(),
             'code' => $stop->getCode(),
             'distance' => $distance
-        ]);
+        ];        
+        if ($request->query->get('withRoutes', 0) == 1) {
+            $data['routes'] = $this->getRoutesForStop($stop, $em, $dateTimeHelper);
+        }
+        return $this->json($data);
     }
 
     /**
